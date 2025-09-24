93138235bac4ccbcdbacdc5c944743ef1b2b8eb6
anerthy <112020687+anerthy@users.noreply.github.com>
2025-09-23 18:13:26 -0600

    new favicon

--- consts.ts
+++ consts.ts
@@ -3,5 +3,5 @@ export const API_URL = 'https://pokeapi.co/api/v2/pokemon?limit=151';
 export const SITE_INFO = {
   title: 'Pokédex',
   description: 'A simple Pokédex built with Astro',
-  image: '/og-image.png',
+  image: '/pokeball.png',
 }
